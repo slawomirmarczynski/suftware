8a6e6f1ab20ad5e9c175af0b7961b7ac8ec3e6f7
Ammar Tareen <tareen@QB-Physics-Ammar.local>
2018-02-21 09:52:33 -0500

    Updated documentation, fit function added to class Deft1D

--- Deft_Classes.py
+++ Deft_Classes.py
@@ -1,6 +1,7 @@
 from scipy import interpolate
 import scipy as sp
 from deft_code import utils
+from deft_code import deft_1d
 
 
 class Field1D:
@@ -17,10 +18,8 @@ class Field1D:
 
     Attributes:
     ----------
-    bin-centers
-    interpolated_phi
-    xmin
-    xmax
+    bin-centers (array like) corresponds to the x values where phi is evaluated
+    interpolated_phi (object) the interpolated field
 
     Methods:
     -------
@@ -41,8 +40,6 @@ class Field1D:
         self.interpolated_phi = interpolate.interp1d(self.bin_centers, phi)
         # delete counts since they are not needed in this class
         del counts
-        self.xmin = bbox[0]
-        self.xmax = bbox[1]
 
     # method to evaluate the interpolant at a given x value
     def evaluate(self,x):
@@ -65,10 +62,13 @@ class Density1D:
 
     Attributes:
     ----------
-    Field1D
-    xs
-    h
-    Z
+    Field1D: (class) this field attribute is an input to the constructor
+        during initialization of the Density1D instance. It's an
+        attribute so that it may be used in the evaluate function
+        for all instances of Density1D.
+    xs (array like) corresponds to the x values where Q (or phi) is evaluated
+    h  (float) bin width
+    Z  (float) partition/normalization function
 
     Methods:
     -------
@@ -107,11 +107,55 @@ class Deft1D:
     set_params()
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, message='', should_succeed=True, feed_data=False, data_fed=None, Q_true_func=None,
+                 data_type='wide', N=100, data_seed=None, G=100, alpha=3, bbox=[-6,6],bbox_state=1,input_type='simulated', periodic=False,
+                 Z_eval='Lap', num_Z_samples=0, DT_MAX=1.0, print_t=False, tollerance=1E-6, resolution=0.1,
+                 deft_seed=None, pt_method=None, num_pt_samples=0, fix_t_at_t_star=True):
+
+        # set class attributes
+        self.message = message
+        self.should_succeed = should_succeed
+        self.feed_data = feed_data
+        self.data_fed = data_fed
+        self.Q_true_func = Q_true_func
+        self.data_type = data_type
+        self.N = N
+        self.data_seed = data_seed
+        self.G = G
+        self.alpha = alpha
+        self.bbox = bbox
+        self.bbox_state = bbox_state
+        self.periodic = periodic
+        self.Z_eval = Z_eval
+        self.num_Z_samples = num_Z_samples
+        self.DT_MAX = DT_MAX
+        self.print_t = print_t
+        self.tollerance = tollerance
+        self.resolution = resolution
+        self.deft_seed = deft_seed
+        self.pt_method = pt_method
+        self.num_pt_samples = num_pt_samples
+        self.fix_t_at_t_star = fix_t_at_t_star
+        self.input_type = input_type
+
+        self.outcome_good = False
 
     def fit(self):
-        pass
+
+        # Run deft_1d
+        try:
+            self.results = deft_1d.run(data=self.data, G=self.G, alpha=self.alpha, bbox=self.bbox,
+                                       periodic=self.periodic, Z_eval=self.Z_eval, num_Z_samples=self.num_Z_samples,
+                                       DT_MAX=self.DT_MAX, print_t=self.print_t, tollerance=self.tollerance,
+                                       resolution=self.resolution, deft_seed=self.deft_seed, pt_method=self.pt_method,
+                                       num_pt_samples=self.num_pt_samples, fix_t_at_t_star=self.fix_t_at_t_star)
+            print('Succeeded!  t_star = %.2f' % self.results.t_star)
+            print(self.pt_method)
+            self.outcome_good = self.should_succeed
+
+        except:
+            print('Deft fit failed')
+
 
     def get_QStar(self):
         pass
