c196ec53470b775c5846b6c09a234a594ac8b8c9
Ammar Tareen <tareen@qbphysicsammar.cshl.edu>
2018-02-22 15:51:08 -0500

    Slightly updated and improved api after code review with Wei-chia

--- deft_classes.py
+++ deft_classes.py
@@ -3,108 +3,10 @@ import scipy as sp
 from deft_code import utils
 from deft_code import deft_1d
 
-
-class Field1D:
-    """
-    This class takes a 1d field object evaluated on a grid and returns
-    and instance of the Field1d class containing an interpolated field.
-    The interpolation is done via scipy's interpolate package.
-
-    parameters:
-    ----------
-    phi: (...,N,...)
-        array_like A 1-D array of real values.
-    G: (int)
-        number of grid points on which phi is evaluated
-    bbox: ([int,int])
-        list that specifies interpolation as xmin, xmax
-
-    Attributes:
-    ----------
-    bin-centers: (array like)
-        corresponds to the x values where phi is evaluated
-    interpolated_phi: (object)
-        the interpolated field
-
-    Methods:
-    -------
-    evaluate(x):
-        returns the interpolant at x. Returns zero if x is
-        outside the interpolation range
-
-    Usage:
-    -----
-    field_1d_Object = Field1D(phi_input,grid_size,bbox)
-    field_1d_Object.evaluate(x)
-    """
-
-    # constructor: calls the interp1d function which default to
-    def __init__(self,phi, G, bbox):
-
-        counts, self.bin_centers = utils.histogram_counts_1d(phi, G, bbox)
-        self.interpolated_phi = interpolate.interp1d(self.bin_centers, phi)
-        # delete counts since they are not needed in this class
-        del counts
-
-    # method to evaluate the interpolant at a given x value
-    def evaluate(self,x):
-        # returns the interpolant at x
-        try:
-            return self.interpolated_phi.__call__(x)
-        # handle value
-        except ValueError:
-            print(" The x value is out of the interpolation range.")
-
-
-class Density1D:
-    """
-    This class forms a Density object (Q) based on the
-    interpolated field object from the Field1D class
-
-    parameters:
-    ----------
-    Field1D: (class)
-        instance of class Field1D
-
-    Attributes:
-    ----------
-    Field1D: (class)
-        this field attribute is an input to the constructor
-        during initialization of the Density1D instance. It's an
-        attribute so that it may be used in the evaluate function
-        for all instances of Density1D.
-    xs: (array like)
-        corresponds to the x values where Q (or phi) is evaluated
-    h:  (float)
-        bin width
-    Z:  (float)
-        partition/normalization function
-
-    Methods:
-    -------
-    evaluate(x):
-        returns Q(x) at x. Returns zero if x is outside the
-        interpolation range
-
-    Usage:
-    -----
-    density1DObject = Density1D(fieldObject)
-    density1DObject.evaluate(x)
-    """
-
-    def __init__(self, Field1D):
-        self.Field1D = Field1D
-        # the 0.1's are heuristic numbers that seem to work well with interp1d
-        self.xs = self.Field1D.bin_centers
-        self.h = self.xs[1]-self.xs[0]
-        self.Z = sp.sum(self.h * sp.exp(-self.Field1D.evaluate(self.xs)))
-
-    def evaluate(self,x):
-        try:
-            # return Q(x)
-            return sp.exp(-self.Field1D.evaluate(x)) / self.Z
-        except:
-            print("Error: please x value out of interpolation range")
+"""
+This module contains the classes Deft1D, Field1D, and Density1D
+See doc-strings for each class for details
+"""
 
 
 class Deft1D:
@@ -219,6 +121,8 @@ class Deft1D:
                 bbox_right = int(np.max(self.data) + 0.2 * data_spread)
                 self.bbox = [bbox_left, bbox_right]
 
+            # *** NOTE *** inputs check will likely go here.
+
             self.results = deft_1d.run(data=self.data, G=self.G, alpha=self.alpha, bbox=self.bbox,
                                        periodic=self.periodic, Z_eval=self.Z_eval, num_Z_samples=self.num_Z_samples,
                                        DT_MAX=self.DT_MAX, print_t=self.print_t, tollerance=self.tolerance,
@@ -226,18 +130,24 @@ class Deft1D:
                                        num_pt_samples=self.num_pt_samples, fix_t_at_t_star=self.fix_t_at_t_star)
 
             print('Deft1D ran successfully')
-            # should fit return results?
             return self.results
 
-
+        # this should be more specific
         except:
             # include include message with more details here
+            # *** Ask what to do ***
             print('Deft fit failed')
 
-    def get_results(self):
-        if self.results is not None:
-            # return the dictionary containing results
+    def get_results(self, key=None):
+        if self.results is not None and key is None:
+            # return the dictionary containing results if no key provided
             return self.results.__dict__
+        elif self.results is not None and key is not None:
+            try:
+                #return self.results.__dict__.get(key)
+                return self.results.__getattribute__(key)
+            except AttributeError as e:
+                print("Get results:",e)
         else:
             print("Get Results: Deft results are none. Please run fit first.")
 
@@ -273,39 +183,157 @@ class Deft1D:
                 self.__setattr__(key, kwargs[key])
 
 
+class Field1D:
+    """
+    This class takes a 1d field object evaluated on a grid and returns
+    and instance of the Field1d class containing an interpolated field.
+    The interpolation is done via scipy's interpolate package.
+
+    parameters:
+    ----------
+    phi: (...,N,...)
+        array_like A 1-D array of real values.
+    G: (int)
+        number of grid points on which phi is evaluated
+    bbox: ([int,int])
+        list that specifies interpolation as xmin, xmax
+
+    Attributes:
+    ----------
+    bin-centers: (array like)
+        corresponds to the x values where phi is evaluated
+    interpolated_phi: (object)
+        the interpolated field
+
+    Methods:
+    -------
+    evaluate(x):
+        returns the interpolant at x. Returns zero if x is
+        outside the interpolation range
+
+    Usage:
+    -----
+    field_1d_Object = Field1D(phi_input,grid_size,bbox)
+    field_1d_Object.evaluate(x)
+    """
+
+    # constructor: calls the interp1d function which default to
+    def __init__(self,phi, G, bbox):
+
+        counts, self.bin_centers = utils.histogram_counts_1d(phi, G, bbox)
+        self.interpolated_phi = interpolate.interp1d(self.bin_centers, phi)
+        # delete counts since they are not needed in this class
+        del counts
+
+    # method to evaluate the interpolant at a given x value
+    def evaluate(self,x):
+        # returns the interpolant at x
+        try:
+            return self.interpolated_phi.__call__(x)
+        # handle value
+        except ValueError:
+            print(" The x value is out of the interpolation range.")
+
+
+class Density1D:
+    """
+    This class forms a Density object (Q) based on the
+    interpolated field object from the Field1D class
+
+    parameters:
+    ----------
+    Field1D: (class)
+        instance of class Field1D
+
+    Attributes:
+    ----------
+    Field1D: (class)
+        this field attribute is an input to the constructor
+        during initialization of the Density1D instance. It's an
+        attribute so that it may be used in the evaluate function
+        for all instances of Density1D.
+    xs: (array like)
+        corresponds to the x values where Q (or phi) is evaluated
+    h:  (float)
+        bin width
+    Z:  (float)
+        partition/normalization function
+
+    Methods:
+    -------
+    evaluate(x):
+        returns Q(x) at x. Returns zero if x is outside the
+        interpolation range
+
+    Usage:
+    -----
+    density1DObject = Density1D(fieldObject)
+    density1DObject.evaluate(x)
+    """
+
+    def __init__(self, Field1D):
+        self.Field1D = Field1D
+        # the 0.1's are heuristic numbers that seem to work well with interp1d
+        self.xs = self.Field1D.bin_centers
+        self.h = self.xs[1]-self.xs[0]
+        self.Z = sp.sum(self.h * sp.exp(-self.Field1D.evaluate(self.xs)))
+
+    def evaluate(self,x):
+        try:
+            # return Q(x)
+            return sp.exp(-self.Field1D.evaluate(x)) / self.Z
+        except:
+            print("Error: please x value out of interpolation range")
+
+
+import sys
 # Use cases/tests
 import numpy as np
 # load data
 data = np.loadtxt('./data/old_faithful_eruption_times.dat').astype(np.float)
 # initialize deft object
-deft = Deft1D(data)
+deft = Deft1D(data,pt_method='Lap+W')
 # run fit
 deft.fit()
+
+# get one parameter value
+#print(deft.get_params('G'))
 
 # get all parameters
 deft.get_params()
 
-# get deft results
-deft.get_results()
-
-# get Q_samples
-deft.get_Qsampled()
-
 # get parameter by key
 deft.get_params('resolution')
 
+# get Q_star
+deft.get_Qstar()
+
+# get Q_samples
+#deft.get_Qsampled()
+
+# get deft results
+deft.get_results()
+
+# get deft result by key
+deft.get_results('phi_star')
+
+# access particular results pythonically
+#print(deft.get_results()['phi_star'])
+
 # set individual parameters by key, value
-deft.set_params('G',10)
+#deft.set_params('G',10)
 
-# set params via dict
-d = {"G":10,"alpha":2}
-deft.set_params(**d)
-#print(deft.get_params())
+# set parameters via dictionary
+#d = {"G":10,"alpha":2}
+#deft.set_params(**d)
+#print(deft.get_Qsampled())
 
-#field = Field1D(deft.get_results()['phi_star'], deft.get_params('G'), deft.get_params('bbox'))
+
+field = Field1D(deft.get_results()['phi_star'], deft.get_params('G'), deft.get_params('bbox'))
 #print(field.evaluate(0.75))
-#density = Density1D(field)
-#print(density.evaluate(0.75))
+
+density = Density1D(field)
+#density.evaluate(0.5)
 
 #print(deft.get_results()['phi_star'])
 #print(deft.get_params('alpha'))
