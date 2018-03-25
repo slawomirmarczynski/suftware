ed029201ebc146a218f6fc65d9917d71c2facc53
Justin B. Kinney <jkinney@cshl.edu>
2018-03-25 17:36:35 -0400

    Simplified interface to simulate_data_1d and moved simulate_data_*d.py to deft_code

--- suftware.py
+++ suftware.py
@@ -5,3 +5,5 @@
 from deft_code.deft_1d import Deft1D
 from deft_code.density_1d import Density1D
 from deft_code.field_1d import Field1D
+
+from deft_code.simulate_data_1d import run as simulate_data_1d
