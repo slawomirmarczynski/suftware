2e74aab88f227a214e03ea4ce53f8b9506aafa95
Ammar Tareen <atareen64@gmail.com>
2019-08-19 12:12:54 -0400

    fixing comb bug in suftware with later SciPy versions. See https://github.com/jbkinney/suftware/issues/10. Suftware 0.15 is released on pypi

--- src/laplacian.py
+++ src/laplacian.py
@@ -3,7 +3,12 @@ import numpy as np
 from scipy.sparse import csr_matrix, diags
 from scipy.sparse.linalg import spsolve
 from scipy.linalg import det, eigh, qr
-from scipy.misc import comb
+# AT: fixing SciPy comb import bug: 8_19_2019
+# for SciPy versions >= 0.19
+try:  
+    from scipy.special import comb
+except ImportError:
+    from scipy.misc import comb
 import pickle
 
 from src import utils
