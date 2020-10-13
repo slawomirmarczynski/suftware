Fixed entropy sign and scipy.misc.comb -> scipy.special.comb

src/DensityEstimator.py
@@ -596,7 +596,7 @@ class DensityEstimator:
             h = self.grid_spacing
             eps = 1E-10
             assert (all(Q >= 0))
-            return np.sum(h * Q * np.log2(Q + eps))
+            return -np.sum(h * Q * np.log2(Q + eps))
 
         def mean(Q):
             x = self.grid

src/laplacian.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.sparse import csr_matrix, diags
 from scipy.sparse.linalg import spsolve
 from scipy.linalg import det, eigh, qr
-from scipy.misc import comb
+from scipy.special import comb
 import pickle
 
 from src import utils