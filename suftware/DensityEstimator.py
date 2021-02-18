3d816e48642ef8b2d9afb4c31aedd5b727706186
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2021-02-18 23:35:41 +0100

    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- suftware/DensityEstimator.py
+++ suftware/DensityEstimator.py
@@ -11,30 +11,25 @@ import deft_core
 import laplacian
 from utils import ControlledError, enable_graphics, check, handle_errors,\
     clean_numerical_input, LISTLIKE
-from DensityEvaluator import DensityEvaluator
+from DensityEvaluator import InterpolatedPDF
 
 
 SMALL_NUM = 1E-6  #? arbitrary, non-coherent with the `utils` module. 
-MAX_NUM_GRID_POINTS = 1000
+MAX_NUM_GRID_POINTS = 1000 #? what for? I have BIG COMPUTER - and...
 DEFAULT_NUM_GRID_POINTS = 100
 MAX_NUM_POSTERIOR_SAMPLES = 1000
 MAX_NUM_SAMPLES_FOR_Z = 1000
 
 
 
-class DensityEstimator:
+class SimpleDensityEstimator:
     """
     """
 
-    def __init__(self,
-                 data,
-                 grid=None,
-                 step=None,
-                 num_grid_points=None,
-                 bounding_box=None,
+    def __init__(self, data,
+                 num_grid_points=DEFAULT_NUM_GRID_POINTS,
                  alpha=3,
                  periodic=False,
-                 num_posterior_samples=100,
                  max_t_step=1.0,
                  tolerance=1E-6,
                  resolution=0.1,
@@ -47,9 +42,11 @@ class DensityEstimator:
 
         # Record other inputs as class attributes
         self.alpha = alpha
-        self.grid = grid
-        self.step = step
+        self.grid = None
+        self.step = None
         self.num_grid_points = num_grid_points
+        self.low = None
+        self.high = None
         self.bounding_box = bounding_box
         self.periodic = periodic
         self.Z_evaluation_method = evaluation_method_for_Z
@@ -59,7 +56,6 @@ class DensityEstimator:
         self.tolerance = tolerance
         self.seed = seed
         self.resolution = resolution
-        self.num_posterior_samples = num_posterior_samples
         self.sample_only_at_l_star = sample_only_at_l_star
         self.max_log_evidence_ratio_drop = max_log_evidence_ratio_drop
         self.data = data
@@ -68,11 +64,37 @@ class DensityEstimator:
         # Data should be numeric and finite. Thus any NAN/INF values are removed
         # - notice that it is possible that an exception may be raised.
         #
+        #? Remove it - it is not pythonic - just assume that data MUST BE VALID
+        #
         data = np.mat(data, float)
         data = data[np.isfinite(data)]
 
-        # Choose grid
-        self._set_grid()
+        # init_bbox
+        #
+        PADDING_FACTOR = 0.2
+        x1 = np.min(data)
+        x2 = np.max(data)
+        self.low = x1 - (x2 - x1) * PADDING_FACTOR
+        self.high = x2 + (x2 - x1) * PADDING_FACTOR
+        if self.low < 0 and x1 >= 0:
+            self.low = 0
+        if self.high > 0 and x2 > 0:
+            self.high = 0
+
+        # Define a grid based on self.low and self.high (i.e. based on the 
+        # bounding box): set grid spacing, define grid padding, define grid to
+        # be centered in bounding box, set bin edges.
+        #
+        self.step = (self.high - self.low) / self.num_grid_points
+        self.grid_padding = self.step / 2
+        grid_start = self.low + self.grid_padding
+        grid_stop = self.high - self.grid_padding
+        self.grid = np.linspace(
+            grid_start, grid_stop * (1 + SMALL_NUM), # For safety?!?!? WTF?!
+            self.num_grid_points)
+        self.bin_edges = np.concatenate(
+            ([self.low], self.grid[:-1] + self.step / 2, [self.high]))
+
 
         # Fit to data
         self._run()
@@ -87,141 +109,11 @@ class DensityEstimator:
 
         # Compute evaluator for density
         #
-        self.density_func = DensityEvaluator(self.phi_star_values,
-                                             self.grid,
-                                             self.bounding_box)
-
-        # Compute optimal density at grid points
-        #
-        self.values = self.evaluate(self.grid)
-
-        # If any posterior samples were taken
-        #
-        if num_posterior_samples > 0:
-
-            # Save sampled phi values and weights
-            self.sample_field_values = self.results.phi_samples
-            self.sample_weights = self.results.phi_weights
-
-            # Compute evaluator for all posterior samples
-            self.sample_density_funcs = [
-                DensityEvaluator(field_values=self.sample_field_values[:, k],
-                                 grid=self.grid,
-                                 bounding_box=self.bounding_box)
-                for k in range(self.num_posterior_samples)
-            ]
-
-            # Compute sampled values at grid points
-            # These are NOT resampled
-            self.sample_values = self.evaluate_samples(self.grid,
-                                                       resample=False)
-
-            # Compute effective sample size and efficiency
-            self.effective_sample_size = np.sum(self.sample_weights)**2 \
-                                        / np.sum(self.sample_weights**2)
-            self.effective_sampling_efficiency = \
-                self.effective_sample_size / self.num_posterior_samples
-
+        self.pdf = InterpolatedPDF(
+            self.results.phi_star, self.grid, self.low, self.high)
 
     @handle_errors
-    def evaluate(self, x):
-        """
-        Evaluate the optimal (i.e. MAP) density at the supplied value(s) of x.
-
-        parameters
-        ----------
-
-        x: (number or list-like collection of numbers)
-            The locations in the data domain at which to evaluate the MAP
-            density.
-
-        returns
-        -------
-
-        A float or 1D np.array representing the values of the MAP density at
-        the specified locations.
-        """
-
-        # Clean input
-        x_arr, is_number = clean_numerical_input(x)
-
-        # Compute distribution values
-        values = self.density_func.evaluate(x_arr)
-
-        # If input is a single number, return a single number
-        if is_number:
-            values = values[0]
-
-        # Return answer
-        return values
-
-
-    @handle_errors
-    def evaluate_samples(self, x, resample=True):
-        """
-        Evaluate sampled densities at specified locations.
-
-        parameters
-        ----------
-
-        x: (number or list-like collection of numbers)
-            The locations in the data domain at which to evaluate sampled
-            density.
-
-        resample: (bool)
-            Whether to use importance resampling, i.e., should the values
-            returned be from the original samples (obtained using a Laplace
-            approximated posterior) or should they be resampled to
-            account for the deviation between the true Bayesian posterior
-            and its Laplace approximation.
-
-        returns
-        -------
-
-        A 1D np.array (if x is a number) or a 2D np.array (if x is list-like),
-        representing the values of the posterior sampled densities at the
-        specified locations. The first index corresponds to values in x, the
-        second to sampled densities.
-        """
-
-        # Clean input
-        x_arr, is_number = clean_numerical_input(x)
-
-        # Check resample type
-        check(isinstance(resample, bool),
-              'type(resample) = %s. Must be bool.' % type(resample))
-
-        # Make sure that posterior samples were taken
-        check(self.num_posterior_samples > 0,
-              'Cannot evaluate samples because no posterior samples'
-              'have been computed.')
-
-        assert(len(self.sample_density_funcs) == self.num_posterior_samples)
-
-        # Evaluate all sampled densities at x
-        values = np.array([d.evaluate(x_arr) for d
-                           in self.sample_density_funcs]).T
-
-        # If requested, resample columns of values array based on
-        # sample weights
-        if resample:
-            probs = self.sample_weights / self.sample_weights.sum()
-            old_cols = np.array(range(self.num_posterior_samples))
-            new_cols = np.random.choice(old_cols,
-                                        size=self.num_posterior_samples,
-                                        replace=True,
-                                        p=probs)
-            values = values[:, new_cols]
-
-        # If number was passed as input, return 1D np.array
-        if is_number:
-            values = values.ravel()
-
-        return values
-
-    @handle_errors
-    def get_stats(self, use_weights=True,
-                  show_samples=False):
+    def get_stats(self, use_weights=True, show_samples=False):
         """
         Computes summary statistics for the estimated density
 
@@ -359,8 +251,9 @@ class DensityEstimator:
         """
 
         # Extract information from Deft1D object
-        data = self.data
+
         G = self.num_grid_points
+        self.low = None,
         h = self.step
         alpha = self.alpha
         periodic = self.periodic
@@ -375,26 +268,16 @@ class DensityEstimator:
         fix_t_at_t_star = self.sample_only_at_l_star
         max_log_evidence_ratio_drop = self.max_log_evidence_ratio_drop
 
-        # Start clock
+        # Start clock - anyway it is not a good idea - the computation time is
+        # not a revelant result.
+        #
         clock = ConsumedTimeTimer()
-        start_time = clock()
-
-        # If deft_seed is specified, set it
-        if not (deft_seed is None):
-            np.random.seed(deft_seed)
-        else:
-            np.random.seed(None)
 
         # Create Laplacian
-        laplacian_start_time = clock()
-        if periodic:
-            op_type = '1d_periodic'
-        else:
-            op_type = '1d_bilateral'
-        Delta = laplacian.Laplacian(op_type, alpha, G)
-        laplacian_compute_time = clock() - laplacian_start_time
-        if print_t:
-            print('Laplacian computed de novo in %f sec.'%laplacian_compute_time)
+        clock.tic()
+        Delta = laplacian.Laplacian(1, self.periodic, self.alpha, G)
+        print('laplace operator constructed')
+        clock.toc()
 
         # Get histogram counts and grid centers
 
@@ -496,166 +379,4 @@ class DensityEstimator:
         # Set cleaned data
         self.data = data
 
-
-    def _set_grid(self):
-        """
-        Sets the grid based on user input
-        """
-
-        data = self.data
-        grid = self.grid
-        step = self.step
-        num_grid_points = self.num_grid_points
-#?        bounding_box = self.bounding_box
-        low = self.low
-        high = self.high
-
-        alpha = self.alpha
-
-        # When this method is called there are only two possibilities: 
-        # we have got no grid or we have got a grid.
-        #
-        if grid is None:
-
-            ### Next, define grid based on bounding box ###
-
-            # If step is specified
-            if (step is not None):
-                assert isinstance(step, float)
-                assert np.isfinite(step)
-                assert step > 0
-
-                # Set number of grid points
-                num_grid_points = np.floor(box_size/step).astype(int)
-
-                # Check num_grid_points isn't too small
-                check(2*self.alpha <= num_grid_points,
-                      'Using step = %f ' % step +
-                      'produces num_grid_points = %d, ' % num_grid_points +
-                      'which is too small. Reduce step or do not set.')
-
-                # Check num_grid_points isn't too large
-                check(num_grid_points <= MAX_NUM_GRID_POINTS,
-                      'Using step = %f ' % step +
-                      'produces num_grid_points = %d, ' % num_grid_points +
-                      'which is too big. Increase step or do not set.')
-
-                # Define grid padding
-                # Note: step/2 <= grid_padding < step
-                grid_padding = (box_size - (num_grid_points-1)*step)/2
-                assert (step/2 <= grid_padding < step)
-
-                # Define grid to be centered in bounding box
-                grid_start = lower_bound + grid_padding
-                grid_stop = upper_bound - grid_padding
-                grid = np.linspace(grid_start,
-                                   grid_stop * (1 + SMALL_NUM), # For safety
-                                   num_grid_points)
-
-            # Otherwise, if num_grid_points is specified
-            elif (num_grid_points is not None):
-                assert isinstance(num_grid_points, int)
-                assert 2*alpha <= num_grid_points <= MAX_NUM_GRID_POINTS
-
-                # Set grid spacing
-                step = box_size / num_grid_points
-
-                # Define grid padding
-                grid_padding = step/2
-
-                # Define grid to be centered in bounding box
-                grid_start = lower_bound + grid_padding
-                grid_stop = upper_bound - grid_padding
-                grid = np.linspace(grid_start,
-                                   grid_stop * (1 + SMALL_NUM), # For safety
-                                   num_grid_points)
-
-            # Otherwise, set step and num_grid_points based on data
-            else:
-                assert isinstance(data, np.ndarray)
-                assert all(np.isfinite(data))
-                assert min(data) < max(data)
-
-                # Compute default grid spacing
-                default_step = box_size/DEFAULT_NUM_GRID_POINTS
-
-                # Set minimum number of grid points
-                min_num_grid_points = 2 * alpha
-
-                # Set minimum grid spacing
-                data.sort()
-                diffs = np.diff(data)
-                min_step = min(diffs[diffs > 0])
-                min_step = min(min_step,
-                                       box_size/min_num_grid_points)
-
-                # Set step
-                step = max(min_step, default_step)
-
-                # Set number of grid points
-                num_grid_points = np.floor(box_size/step).astype(int)
-
-                # Set grid padding
-                grid_padding = step/2
-
-                # Define grid to be centered in bounding box
-                grid_start = lower_bound + grid_padding
-                grid_stop = upper_bound - grid_padding
-                grid = np.linspace(grid_start,
-                                   grid_stop * (1 + SMALL_NUM),  # For safety
-                                   num_grid_points)
-
-
-        if grid is not None:
-
-            # Check and set number of grid points
-            num_grid_points = len(grid)
-            assert(num_grid_points >= 2*alpha)
-
-            # Check and set grid spacing
-            diffs = np.diff(grid)
-            step = diffs.mean()
-            assert (step > 0)
-            assert (all(np.isclose(diffs, step)))
-
-            # Check and set grid bounds
-            grid_padding = step / 2
-            lower_bound = grid[0] - grid_padding
-            upper_bound = grid[-1] + grid_padding
-            bounding_box = np.array([lower_bound, upper_bound])
-            box_size = upper_bound - lower_bound
-
-
-        # Set final grid
-        self.grid = grid
-        self.step = step
-        self.grid_padding = grid_padding
-        self.num_grid_points = num_grid_points
-        self.bounding_box = bounding_box
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.box_size = box_size
-
-
-        # Set bin edges
-        self.bin_edges = np.concatenate(([lower_bound],
-                                         grid[:-1]+step/2,
-                                         [upper_bound]))
-
-    def _init_bbox(self):
-
-        #? Assuming that data is a matrix n-by-2 with x, y values
-
-        PADDING_FACTOR = 0.2
-
-        x1 = np.min(data[:, 0])
-        x2 = np.max(data[:, 0])
-        self.low = x1 - (x2 - x1) * PADDING_FACTOR
-        self.high = x2 + (x2 - x1) * PADDING_FACTOR
-
-        if self.low < 0 and x1 >= 0:
-            self.low = 0
-
-        if self.high > 0 and x2 > 0:
-            self.high = 0
 
