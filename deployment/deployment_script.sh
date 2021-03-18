44e00d13d4ccbf445f5a415093ca9e954938cde2
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2021-03-18 12:11:10 +0100

    Removed notebooks and some other trash
    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- deployment/deployment_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo python setup.py sdist bdist_wheel
-virtualenv your-package-sdist
-source your-package-sdist/bin/activate
-your-package-sdist/bin/pip install dist/suftware-0.1.tar.gz
-cd your-package-sdist
-bin/python
-echo "Ensure package installed correctly by importing it and running a basic test_for_mistake"
-echo " If test_for_mistake successful, run: twine upload pypi dist/suftware-0.1*"
